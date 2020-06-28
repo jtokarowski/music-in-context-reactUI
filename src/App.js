e4c40ccccb0b923e1f3c0b7dbb94d58e5ae2d96a
John Tokarowski <johnt3@bu.edu>
2020-06-28 10:21:55 -0400

    clean up API calls

--- src/App.js
+++ src/App.js
@@ -8,30 +8,27 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      radarChartData:{},
-      lineChartData:{}
     }
   }
  
    componentDidMount(){
-     //this.getChartData();
-
      // grab refresh token from URL
-    // let search = window.location.search;
-    // let params = new URLSearchParams(search);
-    // let data = {
-    //   refresh_token: params.get('refresh_token'),
-    //   mode: params.get('mode'),
-    //   form_data: params.get('form_data')
-    // };
-    //hard coded for testing
+     let search = window.location.search;
+     let params = new URLSearchParams(search);
      let data = {
-      refresh_token: 'AQACS834nGRrzXZySKHt90Nsu7vZWpKCmCM73hGFiva-nDRE5NHgg6q5XTvlH4tDcAfYvCIaLdMN2EerUVS2CyyYgPXR1crpFxtwvFQYqFJh8bhLQuCThQnto-AMhxszuCs',
-      mode: 'playlist',
-      form_data: '4MZTnMUwga6imMXcrepZ7y'
-    };
+       refresh_token: params.get('refresh_token'),
+       mode: params.get('mode'),
+       form_data: params.get('form_data')
+     };
+    // //hard coded for testing
+    //  let data = {
+    //   refresh_token: 'AQACS834nGRrzXZySKHt90Nsu7vZWpKCmCM73hGFiva-nDRE5NHgg6q5XTvlH4tDcAfYvCIaLdMN2EerUVS2CyyYgPXR1crpFxtwvFQYqFJh8bhLQuCThQnto-AMhxszuCs',
+    //   mode: 'playlist',
+    //   form_data: '4MZTnMUwga6imMXcrepZ7y'
+    // };
 
     let posturl = 'https://music-in-context-backend.herokuapp.com/data';
+    //let posturl = 'http://127.0.0.1:7000/data';
     //send a post to the backend API to run the calcs and respond with data
     fetch(posturl,{
         mode: 'cors',
